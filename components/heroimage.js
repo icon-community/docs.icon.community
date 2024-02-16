3b08c6ae66a3dab8d68855ad3ab7100d6cbe42ed
David <111794534+usr-icon-foundation@users.noreply.github.com>
2024-02-16 17:59:59 +0100

    Image fix hero

--- components/heroimage.js
+++ components/heroimage.js
@@ -2,13 +2,14 @@ import Image from 'next/image';
 
 const HeroImage = () => {
   return (
-    <div className='absolute right-0 w-full sm:w-[500px] grayscale-[50%] -rotate-90 sm:rotate-0 opacity-10 sm:opacity-100'>
+    <div className='absolute right-0 h-full w-full sm:w-[50vh] grayscale-[50%] -rotate-90 sm:rotate-0 transition-all opacity-10 md:opacity-40 lg:opacity-100'>
       <Image 
         src='/images/background/hero-bridges.png' 
         alt='Bridges' 
-        width={935} 
-        height={608} 
+        
         className=''
+        layout='fill'
+        objectFit='contain'
       />
     </div>
   );
