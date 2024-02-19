f28f68a7c226fa26b102d69ebeb5c3238df08848
David <111794534+usr-icon-foundation@users.noreply.github.com>
2024-02-19 10:28:12 +0100

    Update heroimage.js

--- components/heroimage.js
+++ components/heroimage.js
@@ -5,11 +5,10 @@ const HeroImage = () => {
     <div className='absolute right-0 h-full w-full sm:w-[50vh] grayscale-[50%] -rotate-90 sm:rotate-0 transition-all opacity-10 md:opacity-40 lg:opacity-100'>
       <Image 
         src='/images/background/hero-bridges.png' 
-        alt='Bridges' 
-        
-        className=''
-        layout='fill'
-        objectFit='contain'
+        alt='Bridges'        
+        className='object-contain'
+        width='935'
+        height='608'
       />
     </div>
   );
