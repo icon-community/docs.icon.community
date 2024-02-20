0528b62e3bc29c6a8271065e90c2f845e99df2e8
FidelVe <43641727+FidelVe@users.noreply.github.com>
2024-02-20 09:52:31 -0400

    added configuration to support latex math equations, and added new tutorial describing reward calculations

--- next.config.js
+++ next.config.js
@@ -1,22 +1,23 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  latex: true
 });
 
 module.exports = withNextra({
   async redirects() {
     return [
       {
-        source: '/cross-chain-communication/integrations',
-        destination: '/cross-chain-communication/active-development',
-        permanent: true, // or false if the redirect is temporary
-      },
-    ]
+        source: "/cross-chain-communication/integrations",
+        destination: "/cross-chain-communication/active-development",
+        permanent: true // or false if the redirect is temporary
+      }
+    ];
   },
   webpack: (config, { isServer }) => {
     if (isServer) {
-      require('./scripts/generate-sitemap');
+      require("./scripts/generate-sitemap");
     }
     return config;
-  },
+  }
 });
