612f765e4f37bd2734b4b922cb1ec4dcd2d0442d
David <111794534+usr-icon-foundation@users.noreply.github.com>
2024-02-20 09:49:34 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,22 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})
+});
 
 module.exports = withNextra({
+  async redirects() {
+    return [
+      {
+        source: '/cross-chain-communication/integrations',
+        destination: '/cross-chain-communication/active-development',
+        permanent: true, // or false if the redirect is temporary
+      },
+    ]
+  },
   webpack: (config, { isServer }) => {
     if (isServer) {
-      require('./scripts/generate-sitemap')
+      require('./scripts/generate-sitemap');
     }
-    return config
-  }
-})
+    return config;
+  },
+});
