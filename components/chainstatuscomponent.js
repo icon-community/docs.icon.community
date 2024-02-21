92d54073b46efa80186195653c3e8ae1f81573e5
David <111794534+usr-icon-foundation@users.noreply.github.com>
2024-02-21 11:32:38 +0100

    Update

--- components/chainstatuscomponent.js
+++ components/chainstatuscomponent.js
@@ -3,15 +3,13 @@ import { activeintegrations } from './activedevelopment';
 
 const ChainStatusComponent = () => {
   return (
-    <div className="grid w-full grid-cols-2 gap-4 mt-8 mb-4 sm:grid-cols-3">
+    <div className="grid w-full grid-cols-2 gap-2 mb-4 sm:gap-4 md:grid-cols-3">
       {activeintegrations.map((chain, index) => (
         <div key={index} className="mb-6">
-          <div className="flex items-center mb-1">
-            <img src={chain.logo} alt={chain.name + " logo"} className="w-6 h-6 mr-2" />
-            <h3 className="text-lg font-semibold capitalize">{chain.name}</h3>
+          <div className="flex items-center mb-1 dark:text-white ">
+            <img src={chain.logo} alt={chain.name + " logo"} className="w-6 h-6 mr-2 " />
+            <h3 className="text-base font-semibold capitalize sm:text-lg">{chain.name}</h3>
           </div>
-          {/* <p>Status: <strong className='capitalize'>{chain.status}</strong></p>
-          <p>Protocol: <strong>{chain.protocol.join(', ') || 'N/A'}</strong></p> */}
         </div>
       ))}
     </div>
