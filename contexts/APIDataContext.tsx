2f6f60a6b7b5587df779b639d1ab3be497bd0434
David <111794534+usr-icon-foundation@users.noreply.github.com>
2024-05-30 11:29:14 +0200

    Update APIDataContext.tsx

--- contexts/APIDataContext.tsx
+++ contexts/APIDataContext.tsx
@@ -5,7 +5,7 @@ import { DataContextType } from "../types/DataTypes";
 
 const APIDataContext = createContext<DataContextType | undefined>(undefined);
 
-const XCALLTRACKER_ENDPOINT = '/api/statistics/total_messages'
+const XCALLTRACKER_ENDPOINT = '/api/statistics/total_messages';
 
 const networks = [
     'icon', 'bsc', 'eth2', 'havah', 'ibc_archway', 'ibc_neutron', 'ibc_injective',
@@ -22,16 +22,18 @@ export const APIDataContextProvider = ({ children }: { children: ReactNode }) =>
         const fetchData = async () => {
             setLoading(true);
             try {
-                
-                const responseTracker = await fetch(XCALLTRACKER_ENDPOINT, { cache: 'no-store' });               
+                const responseTracker = await fetch(XCALLTRACKER_ENDPOINT, { cache: 'no-store' });
                 const responseTrackerData = await responseTracker.json();
-                setTotalMessages(responseTrackerData.data.total)
+                setTotalMessages(responseTrackerData.data.total);
 
                 const networkPromises = networks.map(async (network) => {
-                    const srcResponse = await fetch(`/api/statistics/total_messages?src_network=${network}`, { cache: 'no-store' });
-                    const srcData = await srcResponse.json();
-                    const destResponse = await fetch(`/api/statistics/total_messages?dest_network=${network}`, { cache: 'no-store' });
-                    const destData = await destResponse.json();
+                    const [srcResponse, destResponse] = await Promise.all([
+                        fetch(`/api/statistics/total_messages?src_network=${network}`, { cache: 'no-store' }),
+                        fetch(`/api/statistics/total_messages?dest_network=${network}`, { cache: 'no-store' })
+                    ]);
+                    
+                    const [srcData, destData] = await Promise.all([srcResponse.json(), destResponse.json()]);
+
                     return { network, src: srcData.data.total, dest: destData.data.total };
                 });
 
@@ -43,7 +45,7 @@ export const APIDataContextProvider = ({ children }: { children: ReactNode }) =>
 
                 setNetworkMessages(networkMessages);
             } catch (error) {
-                console.error('Error fetching xcall fees:', error);
+                console.error('Error fetching network messages:', error);
             } finally {
                 setLoading(false);
             }
@@ -52,7 +54,6 @@ export const APIDataContextProvider = ({ children }: { children: ReactNode }) =>
         fetchData();
     }, []);
     
-
     return (
         <APIDataContext.Provider value={{ loading, totalMessages, networkMessages }}>
             {children}
